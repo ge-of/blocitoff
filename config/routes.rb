d7cdb6c8f265141d810f5ae531f9484769a763f7
Geoff Miles <miles.geoff@gmail.com>
2016-08-14 10:50:50 -0400

    Login added WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :todos
+  root 'todos#index'
+  devise_for :models
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
